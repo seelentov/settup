2739796f65b5bb9badfb1f163ed3d845315557dc
Vladislav Komkov <vladislavkomkov@tibbo.com>
2026-01-23 15:34:26 +0500

    Initial commit

---
+++ src/main/java/ru/vladislavkomkov/settup/SettupApplication.java
@@ -0,0 +1,13 @@
+package ru.vladislavkomkov.settup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladislavkomkov/settup/SettupApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladislavkomkov.settup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
