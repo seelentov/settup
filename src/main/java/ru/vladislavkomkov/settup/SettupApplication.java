778db47691c168cabafe7a7a1d33cd4a55f797ac
vladislav.komkov <vladislav.komkov@tibbo.com>
2026-01-26 13:28:37 +0500

    Update SettupApplication.java

--- src/main/java/ru/vladislavkomkov/settup/SettupApplication.java
+++ src/main/java/ru/vladislavkomkov/settup/SettupApplication.java
@@ -11,25 +11,6 @@ import java.nio.file.Paths;
 @SpringBootApplication
 public class SettupApplication {
     public static void main(String[] args) {
-        Path templatesDir = Paths.get("./templates").toAbsolutePath().normalize();
-        System.out.println("Проверка директории шаблонов: " + templatesDir);
-
-        if (Files.exists(templatesDir)) {
-            if (Files.isDirectory(templatesDir)) {
-                System.out.println("Директория существует. Содержимое:");
-                try {
-                    Files.list(templatesDir)
-                            .map(Path::getFileName)
-                            .forEach(System.out::println);
-                } catch (IOException e) {
-                    System.err.println("Ошибка при чтении директории: " + e.getMessage());
-                }
-            } else {
-                System.out.println("Путь существует, но это НЕ директория.");
-            }
-        } else {
-            System.out.println("Директория ./templates НЕ существует.");
-        }
         SpringApplication.run(SettupApplication.class, args);
     }
 }
